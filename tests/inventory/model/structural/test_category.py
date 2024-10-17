c3ab24c57fd217ed5d8dddcd78d54aa95aef8af7
Rafael Moreira <rafael.mota@plathanus.com>
2024-10-17 17:14:39 -0300

    Validate Expected Columns

--- tests/inventory/model/structural/test_category.py
+++ tests/inventory/model/structural/test_category.py
@@ -1,3 +1,9 @@
+import pytest
+from django.db import models
+
+from inventory.models import Category
+
+
 def test_model_structure_table_exists():
     try:
         from inventory.models import Category  # noqa F401
@@ -7,9 +13,24 @@ def test_model_structure_table_exists():
         assert True
 
 
-def test_model_1():
-    return True
+@pytest.mark.parametrize(
+    "model, field_name, expected_type",
+    [
+        (Category, "id", models.AutoField),
+        (Category, "name", models.CharField),
+        (Category, "slug", models.SlugField),
+        (Category, "is_active", models.BooleanField),
+        (Category, "level", models.IntegerField),
+        (Category, "parent", models.ForeignKey),
+    ],
+)
+def test_model_structure_column_data_types(model, field_name, expected_type):
+    assert hasattr(
+        model, field_name
+    ), f"{model.name} model does not have '{field_name}' field"
 
+    field = model._meta.get_field(field_name)
 
-def test_structure_2():
-    return True
+    assert isinstance(
+        field, expected_type
+    ), f"Field {field_name} is not type {expected_type}"
